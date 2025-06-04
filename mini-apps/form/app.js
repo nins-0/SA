cb34c3ba3e2c9519cc38c91103068cf76bfa95f7
nins-0 <2202667@sit.singaporetech.edu.sg>
2025-06-04 09:29:08 +0800

    test 2 way comm

--- mini-apps/form/app.js
+++ mini-apps/form/app.js
@@ -1,7 +1,30 @@
+// function exitApp() {
+//   if (window.ReactNativeWebView) {
+//     window.ReactNativeWebView.postMessage(JSON.stringify({ event: "exit" }));
+//   } else {
+//     alert("Running outside React Native.");
+//   }
+// }
+
 function exitApp() {
-  if (window.ReactNativeWebView) {
-    window.ReactNativeWebView.postMessage(JSON.stringify({ event: "exit" }));
+  sendMessage({ event: 'exit' });
+}
+
+function sendMessage(data) {
+  if (window.ReactNativeWebView && window.ReactNativeWebView.postMessage) {
+    window.ReactNativeWebView.postMessage(JSON.stringify(data));
   } else {
-    alert("Running outside React Native.");
+    alert("Not inside React Native WebView");
   }
 }
+
+// React Native will call this:
+window.receiveMessage = function (data) {
+  console.log("Received from React Native:", data);
+  alert("Got from RN: " + data.message);
+  // Send something back
+  sendMessage({ event: "reply", received: data.message });
+};
+
+// Send ready message
+sendMessage({ event: "ready", time: Date.now() });
