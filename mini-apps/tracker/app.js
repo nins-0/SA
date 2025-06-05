ac8979ce7685f42e8cb4cd7ebb463ad66692c365
nins-0 <2202667@sit.singaporetech.edu.sg>
2025-06-05 09:02:23 +0800

    test location update 2

--- mini-apps/tracker/app.js
+++ mini-apps/tracker/app.js
@@ -8,45 +8,45 @@ function sendMessage(data) {
   }
 }
 
-function startLocationUpdates() {
-  if (navigator.geolocation) {
-    watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        sendMessage({
-          event: "location_update",
-          latitude,
-          longitude,
-          timestamp: Date.now()
-        });
-      },
-      (error) => {
-        sendMessage({
-          event: "location_error",
-          code: error.code,
-          message: error.message
-        });
-      },
-      {
-        enableHighAccuracy: true,
-        maximumAge: 1000,
-        timeout: 10000,
-      }
-    );
-  } else {
-    sendMessage({
-      event: "location_error",
-      message: "Geolocation is not supported"
-    });
-  }
-}
+// function startLocationUpdates() {
+//   if (navigator.geolocation) {
+//     watchId = navigator.geolocation.watchPosition(
+//       (position) => {
+//         const { latitude, longitude } = position.coords;
+//         sendMessage({
+//           event: "location_update",
+//           latitude,
+//           longitude,
+//           timestamp: Date.now()
+//         });
+//       },
+//       (error) => {
+//         sendMessage({
+//           event: "location_error",
+//           code: error.code,
+//           message: error.message
+//         });
+//       },
+//       {
+//         enableHighAccuracy: true,
+//         maximumAge: 1000,
+//         timeout: 10000,
+//       }
+//     );
+//   } else {
+//     sendMessage({
+//       event: "location_error",
+//       message: "Geolocation is not supported"
+//     });
+//   }
+// }
 
-function stopLocationUpdates() {
-  if (watchId !== null) {
-    navigator.geolocation.clearWatch(watchId);
-    watchId = null;
-  }
-}
+// function stopLocationUpdates() {
+//   if (watchId !== null) {
+//     navigator.geolocation.clearWatch(watchId);
+//     watchId = null;
+//   }
+// }
 
 function openMapPage() {
   window.location.href = "map.html";
