63eaa84af8d5e03d0fd43d1c5c51299e4d85c91b
nins-0 <2202667@sit.singaporetech.edu.sg>
2025-07-01 09:30:25 +0800

    location

--- mini-apps/location/app.js
+++ mini-apps/location/app.js
@@ -48,14 +48,42 @@ function sendMessage(data) {
 //   }
 // }
 
-function openMapPage() {
-  window.location.href = "map.html";
+let map, marker;
+
+function initMap(lat, lng) {
+  map = L.map('map').setView([lat, lng], 16);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+
+  marker = L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup("You are here")
+    .openPopup();
 }
 
-function exitApp() {
-  if (window.ReactNativeWebView) {
-    window.ReactNativeWebView.postMessage(JSON.stringify({ event: "exit" }));
+function updateLocation(lat, lng) {
+  if (!map) {
+    initMap(lat, lng);
   } else {
-    alert("Running outside React Native.");
+    marker.setLatLng([lat, lng]);
+    map.setView([lat, lng]);
   }
 }
+
+navigator.geolocation.watchPosition(
+  (pos) => {
+    const { latitude, longitude } = pos.coords;
+    updateLocation(latitude, longitude);
+  },
+  (err) => {
+    alert("Unable to get location: " + err.message);
+  },
+  {
+    enableHighAccuracy: true,
+    maximumAge: 1000,
+    timeout: 10000,
+  }
+);
+
