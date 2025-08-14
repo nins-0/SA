2eb6fdbfd0e990ea7cbba11d4f2b3c86d9a15d81
nins-0 <2202667@sit.singaporetech.edu.sg>
2025-08-14 15:50:14 +0800

    remove not needed stuff from mini location

--- mini-apps/location/app.js
+++ mini-apps/location/app.js
@@ -7,21 +7,21 @@
   let activePolygon = null;
   let shouldLog = false;
   
-  const radiusInput = document.querySelector('input[value="radius"]');
-  const buildingInput = document.querySelector('input[value="building"]');
-  const radiusSelect = document.getElementById('radiusSelect');
-  const buildingSelect = document.getElementById('buildingSelect');
+  // const radiusInput = document.querySelector('input[value="radius"]');
+  // const buildingInput = document.querySelector('input[value="building"]');
+  // const radiusSelect = document.getElementById('radiusSelect');
+  // const buildingSelect = document.getElementById('buildingSelect');
   const toggleBtn = document.getElementById('toggleTracking');
 
-  radiusInput.addEventListener('change', () => {
-    radiusSelect.disabled = false;
-    buildingSelect.disabled = true;
-  });
+  // radiusInput.addEventListener('change', () => {
+  //   radiusSelect.disabled = false;
+  //   buildingSelect.disabled = true;
+  // });
 
-  buildingInput.addEventListener('change', () => {
-    radiusSelect.disabled = true;
-    buildingSelect.disabled = false;
-  });
+  // buildingInput.addEventListener('change', () => {
+  //   radiusSelect.disabled = true;
+  //   buildingSelect.disabled = false;
+  // });
 
   function requestGeofencingZones() {
     sendToMainApp({ type: 'getGeofencingZones' });
@@ -136,61 +136,54 @@
     });
 
 
-    document.getElementById('geofenceBtn').addEventListener('click', () => {
-      const mode = document.querySelector('input[name="geofenceMode"]:checked').value;
-      radiusSelect.disabled = mode !== 'radius';
-      buildingSelect.disabled = mode !== 'building';
+    // document.getElementById('geofenceBtn').addEventListener('click', () => {
+    //   const mode = document.querySelector('input[name="geofenceMode"]:checked').value;
+    //   radiusSelect.disabled = mode !== 'radius';
+    //   buildingSelect.disabled = mode !== 'building';
 
-      document.getElementById('geofenceModal').classList.remove('hidden');
-    });
+    //   document.getElementById('geofenceModal').classList.remove('hidden');
+    // });
 
-    document.getElementById('cancelGeofenceBtn').addEventListener('click', () => {
-      document.getElementById('geofenceModal').classList.add('hidden');
-    });
+    // document.getElementById('cancelGeofenceBtn').addEventListener('click', () => {
+    //   document.getElementById('geofenceModal').classList.add('hidden');
+    // });
 
-    document.getElementById('applyGeofenceBtn').addEventListener('click', () => {
-      document.getElementById('geofenceModal').classList.add('hidden');
+    // document.getElementById('applyGeofenceBtn').addEventListener('click', () => {
+    //   document.getElementById('geofenceModal').classList.add('hidden');
 
-      const mode = document.querySelector('input[name="geofenceMode"]:checked').value;
+    //   const mode = document.querySelector('input[name="geofenceMode"]:checked').value;
 
-      if (circle) map.removeLayer(circle);
-      if (polygonLayer) map.removeLayer(polygonLayer);
-      geofenceMode = mode;
+    //   if (circle) map.removeLayer(circle);
+    //   if (polygonLayer) map.removeLayer(polygonLayer);
+    //   geofenceMode = mode;
 
-      if (mode === 'radius') {
-        currentRadius = parseInt(document.getElementById('radiusSelect').value);
-        circle = L.circle([userLat, userLng], {
-          color: 'orange',
-          fillColor: '#ff8c3fff',
-          fillOpacity: 0.3,
-          radius: currentRadius
-        }).addTo(map);
-      } else {
-        const selectedIndex = parseInt(document.getElementById('buildingSelect').value);
-          if (isNaN(selectedIndex) || !buildingData[selectedIndex]) return;
-        const points = buildingData[selectedIndex].polygon.map(p => [p.latitude, p.longitude]);
-        polygonLayer = L.polygon(points, {
-          color: 'blue',
-          fillColor: '#3b83bd',
-          fillOpacity: 0.3
-        }).addTo(map);
-        activePolygon = points;
-      }
+    //   if (mode === 'radius') {
+    //     currentRadius = parseInt(document.getElementById('radiusSelect').value);
+    //     circle = L.circle([userLat, userLng], {
+    //       color: 'orange',
+    //       fillColor: '#ff8c3fff',
+    //       fillOpacity: 0.3,
+    //       radius: currentRadius
+    //     }).addTo(map);
+    //   } else {
+    //     const selectedIndex = parseInt(document.getElementById('buildingSelect').value);
+    //       if (isNaN(selectedIndex) || !buildingData[selectedIndex]) return;
+    //     const points = buildingData[selectedIndex].polygon.map(p => [p.latitude, p.longitude]);
+    //     polygonLayer = L.polygon(points, {
+    //       color: 'blue',
+    //       fillColor: '#3b83bd',
+    //       fillOpacity: 0.3
+    //     }).addTo(map);
+    //     activePolygon = points;
+    //   }
 
-      updateLocation(userLat, userLng);
-    });
+    //   updateLocation(userLat, userLng);
+    // });
 
   }
 
-  document.getElementById('cancelPastLocationBtn').addEventListener('click', () => {
-    document.getElementById('pastLocationModal').classList.add('hidden');
-  });
-
-  document.getElementById('pastLocationBtn').addEventListener('click', () => {
-
-    populateLocationList();
-
-    document.getElementById('pastLocationModal').classList.remove('hidden');
+  document.getElementById('ChangeTrackingBtn').addEventListener('click', () => {
+    alert('changed');
   });
 
   toggleBtn.addEventListener('click', () => {
@@ -200,124 +193,7 @@
       alert('Location logging stopped');
     }
   });
-
-  // function updateLocation(lat, lng) {
-  //   userLat = lat;
-  //   userLng = lng;
-
-  //   if (!map) {
-  //     initMap(lat, lng);
-  //   } else {
-  //     marker.setLatLng([lat, lng]);
-  //     // map.setView([lat, lng]);
-  //   }
-
-  //   const banner = document.getElementById('statusBanner');
-
-  //   let inside = false;
-  //   let enteredFence = null;
-
-  //   for (let b of buildingData) {
-  //     if (b.polygon.length > 0) {
-  //       const polygonCoords = b.polygon.map(p => [p.latitude, p.longitude]);
-  //       if (pointInPolygon([lat, lng], polygonCoords)) {
-  //         inside = true;
-  //         enteredFence = b.name; 
-  //         break;
-  //       }
-  //     }
-  //   }
-
-  //   if (geofenceMode === 'radius' && circle) {
-  //     const distance = map.distance([lat, lng], circle.getLatLng());
-  //     inside = distance <= currentRadius;
-  //   } else if (geofenceMode === 'building' && activePolygon) {
-  //     inside = pointInPolygon([lat, lng], activePolygon);
-  //   } else {
-  //     banner.textContent = "No geofence set yet";
-  //     banner.style.backgroundColor = "#737575";
-  //     banner.style.color = "#fff";
-  //     return;
-  //   }
-
-  //   banner.textContent = inside
-  //     ? "You are within the geofence"
-  //     : "You are outside the geofence";
-
-  //   banner.style.backgroundColor = inside ? "#7ea387" : "#c48989";
-  //   banner.style.color = "#2b2b2b";
-
-  //   // Log only if inside and the fence is different from last logged
-  //   if (shouldLog && enteredFence) {
-  //     if (lastFenceLogged !== enteredFence) {
-  //       const timestamp = Date.now();
-  //       saveUserInFence(enteredFence, timestamp);
-  //       lastFenceLogged = enteredFence;
-  //     }
-  //   }
-
-  //   // Reset lastFenceLogged when user leaves the fence
-  //   if (!inside) {
-  //     lastFenceLogged = null;
-  //   }
-
-  // }
-
-
-  // let lastFenceLogged = null;
-
-  // function updateLocation(lat, lng) {
-  //   userLat = lat;
-  //   userLng = lng;
-
-  //   if (!map) {
-  //     initMap(lat, lng);
-  //   } else {
-  //     marker.setLatLng([lat, lng]);
-  //   }
-
-  //   const banner = document.getElementById('statusBanner');
-
-  //   let inside = false;
-  //   let enteredFence = null;
-
-  //   // Only check building polygons
-  //   for (let b of buildingData) {
-  //     if (b.polygon.length > 0) {
-  //       const polygonCoords = b.polygon.map(p => [p.latitude, p.longitude]);
-  //       if (pointInPolygon([lat, lng], polygonCoords)) {
-  //         inside = true;
-  //         enteredFence = b.name;
-  //         break;
-  //       }
-  //     }
-  //   }
-
-  //   if (!inside) {
-  //     banner.textContent = "You are outside the geofence";
-  //     banner.style.backgroundColor = "#c48989";
-  //     banner.style.color = "#2b2b2b";
-      
-  //     // If user was inside before, now exited - send exit event once
-  //     if (lastFenceLogged !== null) {
-  //       saveUserInFence(lastFenceLogged, Date.now(), 'Exit');
-  //       lastFenceLogged = null;
-  //     }
-
-  //     return;
-  //   }
-
-  //   banner.textContent = `You are within the geofence: ${enteredFence}`;
-  //   banner.style.backgroundColor = "#7ea387";
-  //   banner.style.color = "#2b2b2b";
-
-  //   // Only send update if logging is enabled and entered a new fence
-  //   if (shouldLog && enteredFence && lastFenceLogged !== enteredFence) {
-  //     saveUserInFence(enteredFence, Date.now(), 'Enter');
-  //     lastFenceLogged = enteredFence;
-  //   }
-  // }
-
+  
   let activeFences = new Set(); // Track all fences currently inside
 
   function updateLocation(lat, lng) {
@@ -432,29 +308,6 @@
     });
   }
 
-  function getAllLocations() {
-    const data = sessionStorage.getItem('locations');
-    return data ? JSON.parse(data) : [];
-  }
 
-  function populateLocationList() {
-    const locationList = document.getElementById('locationList');
-    locationList.innerHTML = ''; // Clear old entries
-
-    const locations = getAllLocations();
-
-    if (!locations.length) {
-      const li = document.createElement('li');
-      li.textContent = 'No locations recorded.';
-      locationList.appendChild(li);
-      return;
-    }
-
-    locations.forEach(({ latitude, longitude, timestamp }) => {
-      const li = document.createElement('li');
-      li.textContent = `${new Date(timestamp).toLocaleString()} - [${latitude.toFixed(5)}, ${longitude.toFixed(5)}]`;
-      locationList.appendChild(li);
-    });
-  }
 
 
